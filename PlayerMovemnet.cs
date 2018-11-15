3b59261109d3c791aac870399feb0e718433da3e
Devyn <24640365+gamer54298@users.noreply.github.com>
2018-11-15 09:31:16 -0800

    Add files via upload

---
+++ PlayerMovemnet.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovemnet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.LeftArrow));
+        {
+            Vector3 Position = this.transform.position;
+            Position.x--;
+            this.transform.position = Position;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Vector3 Position = this.transform.position;
+            Position.x++;
+            this.transform.position = Position;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Vector3 Position = this.transform.position;
+            Position.y++;
+            this.transform.position = Position;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Vector3 Position = this.transform.position;
+            Position.y--;
+            this.transform.position = Position;
+        }
+
+	}
+}
