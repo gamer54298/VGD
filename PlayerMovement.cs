1aa4506c5202de2d1ffde77f4ae7eba7cda077fd
Devyn <24640365+gamer54298@users.noreply.github.com>
2018-11-19 10:07:44 -0800

    Add files via upload
    player code

---
+++ PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float speed;
+    public float jumpforce;
+    private float moveInput;
+
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput = Input.GetAxisRaw("Horizantal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+}
